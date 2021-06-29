a0eef6192d943b202718e87fb45d9d404229b542
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-06-29 11:21:39 -0500

    Created basic dashboard with search functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,7 @@ let autocomplete;
 // Run on web page load
 function init() {
     renderHeader();
+    renderMainContent();
     $('.sidenav').sidenav();
     $('.fixed-action-btn').floatingActionButton();
     searchInputAutoComplete();
@@ -19,6 +20,98 @@ function renderHeader() {
 
     // Append to the header on the page
     $("header").append($headerTitleH1)
+}
+
+function renderMainContent() {
+    // Build location search
+    buildLocationSearch();
+
+    // Build button to access location search
+    let $btnLocationSearch = $("<a>");
+    $btnLocationSearch.attr("id", "addLocations");
+    $btnLocationSearch.attr("href", "#");
+    $btnLocationSearch.attr("data-target", "locationSlideOut");
+    $btnLocationSearch.addClass("sidenav-trigger waves-effect waves-light btn");
+
+    let $btnLocationSearchIcon = $("<i>");
+    $btnLocationSearchIcon.addClass("material-icons left");
+    $btnLocationSearchIcon.text("search");
+
+    $btnLocationSearch.append($btnLocationSearchIcon);
+    $btnLocationSearch.append("Add Locations");
+
+    // Append location search button to main container
+    $(".container").append($btnLocationSearch);
+
+    // Build the footer
+    let $footerDiv = $("<div>");
+    $footerDiv.text("Made by Triple A");
+    $("footer").append($footerDiv);
+}
+
+function buildLocationSearch() {
+    // Build location search slide-out
+    let $locSlideOutDiv = $("<ul>");
+    $locSlideOutDiv.attr("id", "locationSlideOut");
+    $locSlideOutDiv.addClass("sidenav");
+
+    let $locSlidOutSearch = $("<li>");
+    
+    let $locSlidOutSearchDiv = $("<div>");
+    $locSlidOutSearchDiv.addClass("user-view");
+
+    let $locSlidOutSearchForm = $("<form>");
+    $locSlidOutSearchForm.addClass("col s12");
+
+    let $locSlidOutSearchFormDiv = $("<div>");
+    $locSlidOutSearchFormDiv.addClass("row");
+    
+    let $locSlidOutSearchFormInputDiv = $("<div>");
+    $locSlidOutSearchFormInputDiv.addClass("input-field col s12");
+
+    let $locSlidOutSearchFormInput = $("<input>");
+    $locSlidOutSearchFormInput.attr("id", "searchInput");
+    $locSlidOutSearchFormInput.addClass("materialize-textarea");
+
+    let $locSlidOutSearchFormInputA = $("<a>");
+    $locSlidOutSearchFormInputA.attr("id", "addButton");
+    $locSlidOutSearchFormInputA.addClass("btn-floating btn-small halfway-fab waves-effect waves-light disabled");
+
+    let $locSlidOutSearchFormInputAIcon = $("<i>");
+    $locSlidOutSearchFormInputAIcon.addClass("material-icons");
+    $locSlidOutSearchFormInputAIcon.text("add");
+
+    let $locSlidOutSearchFormInputId = $("<input>");
+    $locSlidOutSearchFormInputId.attr("id", "searchInputId");
+    $locSlidOutSearchFormInputId.attr("type", "hidden");
+
+    let $locSlidOutSearchFormInputLAT = $("<input>");
+    $locSlidOutSearchFormInputLAT.attr("id", "searchInputLAT");
+    $locSlidOutSearchFormInputLAT.attr("type", "hidden");
+
+    let $locSlidOutSearchFormInputLNG = $("<input>");
+    $locSlidOutSearchFormInputLNG.attr("id", "searchInputLNG");
+    $locSlidOutSearchFormInputLNG.attr("type", "hidden");
+
+    $locSlidOutSearchFormInputDiv.append($locSlidOutSearchFormInput);
+    $locSlidOutSearchFormInputA.append($locSlidOutSearchFormInputAIcon);
+    $locSlidOutSearchFormInputDiv.append($locSlidOutSearchFormInputA);
+
+    $locSlidOutSearchFormDiv.append($locSlidOutSearchFormInputDiv);
+    $locSlidOutSearchFormDiv.append($locSlidOutSearchFormInputId);
+    $locSlidOutSearchFormDiv.append($locSlidOutSearchFormInputLAT);
+    $locSlidOutSearchFormDiv.append($locSlidOutSearchFormInputLNG);
+
+    $locSlidOutSearchForm.append($locSlidOutSearchFormDiv);
+
+    $locSlidOutSearchDiv.append($locSlidOutSearchForm);
+
+    $locSlidOutSearch.append($locSlidOutSearchDiv);
+
+    $locSlideOutDiv.append($locSlidOutSearch);
+
+    // Append location search slide-out to main container
+    $(".container").append($locSlideOutDiv);
 }
 
 // Enable autocomplete on the search input
@@ -39,10 +132,11 @@ function renderSearchLocations() {
         searchItems = [];
     }
 
-    // TODO: Fix the sort on the searchItems array
-    searchItems.sort(function(a, b) {
-        return a.cityName - b.cityName
-    });
+    // TODO: User Story #4 -> Sort Locations
+    // searchItems.sort(function(a, b) {
+    //     return a.cityName - b.cityName
+    // });
+
 
     for (let i = 0; i < searchItems.length; i++) {
         let $searchResult = $("<li>");
@@ -51,15 +145,16 @@ function renderSearchLocations() {
         let $searchResultA = $("<a>");
         $searchResultA.addClass("subheader");
 
-        let $searchResultI = $("<i>");
-        $searchResultI.addClass("material-icons");
-        $searchResultI.text("location_on");
+        let $searchResultRemove = $("<i>");
+        $searchResultRemove.attr("id", searchItems[i].cityId);
+        $searchResultRemove.addClass("small material-icons");
+        $searchResultRemove.text("delete");
 
-        $searchResultA.append($searchResultI);
+        $searchResultA.append($searchResultRemove);
         $searchResultA.append(searchItems[i].cityName);
         $searchResult.append($searchResultA);
 
-        $("#slide-out").append($searchResult);
+        $("#locationSlideOut").append($searchResult);
     }
 }
 
@@ -67,6 +162,7 @@ function renderSearchLocations() {
 init();
 
 // ------------- Event Listeners -------------
+//Store the new location in local storage once found via google
 document.querySelector("#addButton").addEventListener("click", function(event) {
     // Get search history items from local storage
     let searchItems = JSON.parse(localStorage.getItem("smallTalk_searchLocations"));
@@ -106,10 +202,13 @@ document.querySelector("#addButton").addEventListener("click", function(event) {
     // Store changes back to local storage
     localStorage.setItem("smallTalk_searchLocations", JSON.stringify(searchItems));
 
+    // TODO: User Story #1 -> Location Form Reset
+
     // Render Search location history
     renderSearchLocations();
 });
 
+// Google event listener for the location search field
 google.maps.event.addListener(autocomplete, "place_changed", function() {
     let searchPlace = autocomplete.getPlace();
 
@@ -128,3 +227,6 @@ google.maps.event.addListener(autocomplete, "place_changed", function() {
         $("#addButton").addClass("disabled");
     }
 })
+
+// TODO: User Story #3 -> Delete Locations from Dashboard
+// Need an event handler for individual clicking the delete / trash can button
